ecbcf9a7572435510c959b1d7b20235583c28bc6
hndhy <zwzzebra25@gmail.com>
2016-08-07 18:11:08 +0800

    优化战队列表ui

--- WeighBean/Sections/CoachListCell.m
+++ WeighBean/Sections/CoachListCell.m
@@ -21,40 +21,43 @@
         self.selectionStyle = UITableViewCellSelectionStyleNone;
         self.backgroundColor = [UIColor clearColor];
         
-        UIView *background = [[UIView alloc] initWithFrame:CGRectMake(10, 5, CGRectGetWidth(self.frame)-20, 240)];
+        UIView *background = [[UIView alloc] initWithFrame:CGRectMake(0, 5, CGRectGetWidth(self.frame), 65)];
         background.backgroundColor = [UIColor whiteColor];
         background.layer.cornerRadius = 5;
         background.layer.masksToBounds = YES;
         [self addSubview:background];
         
-        coachIcon = [[UIImageView alloc] initWithFrame:CGRectMake(15, 25, 60, 60)];
+        coachIcon = [[UIImageView alloc] initWithFrame:CGRectMake(15, 15, 40, 40)];
         coachIcon.backgroundColor = [UIColor blueColor];
         coachIcon.image = nil;
         [background addSubview:coachIcon];
         
-        title = [[UILabel alloc] initWithFrame:CGRectMake(coachIcon.right+20, 25, 100, 30)];
+        title = [[UILabel alloc] initWithFrame:CGRectMake(coachIcon.right+10, 10, 100, 20)];
         title.text = @"人教练战队";
         title.font = [UIFont systemFontOfSize:14];
         [background addSubview:title];
         
-        subTitle1 = [[UILabel alloc] initWithFrame:CGRectMake(coachIcon.right+20, title.bottom+5, 150, 30)];
+        subTitle1 = [[UILabel alloc] initWithFrame:CGRectMake(coachIcon.right+10, title.bottom, 55, 30)];
         subTitle1.text = @"活跃人数";
-        subTitle1.font = [UIFont systemFontOfSize:14];
+        subTitle1.font = [UIFont systemFontOfSize:11];
         [background addSubview:subTitle1];
         
-        subTitle2 = [[UILabel alloc] initWithFrame:CGRectMake(subTitle1.right+20, title.bottom+5, 150, 30)];
+        subTitle2 = [[UILabel alloc] initWithFrame:CGRectMake(subTitle1.right+10, title.bottom, 55, 30)];
         subTitle2.text = @"有效率";
-        subTitle2.font = [UIFont systemFontOfSize:14];
+        subTitle2.font = [UIFont systemFontOfSize:11];
         [background addSubview:subTitle2];
         
-        stataLbl = [[UILabel alloc] initWithFrame:CGRectMake(self.frame.size.width-160, 20, 150, 20)];
+        stataLbl = [[UILabel alloc] initWithFrame:CGRectMake(title.right+50, 10, 150, 20)];
         stataLbl.text = @"状态";
         stataLbl.font = [UIFont systemFontOfSize:14];
         [background addSubview:stataLbl];
         
         joinBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        joinBtn.frame = CGRectMake(stataLbl.left,stataLbl.bottom+5,80,40);
+        joinBtn.frame = CGRectMake(stataLbl.left,stataLbl.bottom+5,80,20);
         joinBtn.backgroundColor = [UIColor blueColor];
+        [joinBtn setTitle:@"加入战队" forState:UIControlStateNormal];
+        joinBtn.titleLabel.tintColor = [UIColor whiteColor];
+        joinBtn.titleLabel.font = [UIFont systemFontOfSize:12];
         [joinBtn addTarget:self action:@selector(btnAction:) forControlEvents:UIControlEventTouchUpInside];
         [background addSubview:joinBtn];
         
@@ -62,6 +65,7 @@
         coachTypeLbl.text = @"状态";
         coachTypeLbl.font = [UIFont systemFontOfSize:14];
         [background addSubview:coachTypeLbl];
+        coachTypeLbl.hidden = YES;
     }
     return self;
 }
