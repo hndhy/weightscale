eca38bb55930f1fd3ab54bae75ad416427c9163b
seal <zwzzebra@gmail.com>
2016-08-24 00:34:19 +0800

    14

---
+++ Build/Intermediates/WeighBean.build/Debug-iphonesimulator/WeighBean.build/Objects-normal/x86_64/CheckInImagePickerCell.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/seal/weightscale/WeighBean/Sections/CheckInImagePickerCell.m \
+  /Users/seal/weightscale/WeighBean/Sections/CheckInImagePickerCell.h \
+  /Users/seal/weightscale/WeighBean/General/Categories/NSDictionary+GetValue.h
---
+++ Build/Intermediates/WeighBean.build/Debug-iphonesimulator/WeighBean.build/Objects-normal/x86_64/CheckInImgPickerViewController.d
@@ -0,0 +1,27 @@
+dependencies: \
+  /Users/seal/weightscale/WeighBean/Sections/CheckInImgPickerViewController.m \
+  /Users/seal/weightscale/WeighBean/Sections/CheckInImgPickerViewController.h \
+  /Users/seal/weightscale/WeighBean/Model/ModelHandlers/HTBaseModelHandler.h \
+  /Users/seal/weightscale/WeighBean/Model/Models/HTAbstractDataSource.h \
+  /Users/seal/weightscale/WeighBean/Network/HTApiClient.h \
+  /Users/seal/weightscale/WeighBean/Model/ModelObjects/BaseResponse.h \
+  /Users/seal/weightscale/WeighBean/General/Common/HTBaseViewController.h \
+  /Users/seal/weightscale/WeighBean/Macro/AppMacro.h \
+  /Users/seal/weightscale/WeighBean/Macro/UtilsMacro.h \
+  /Users/seal/weightscale/WeighBean/Macro/VersionMacro.h \
+  /Users/seal/weightscale/WeighBean/General/Categories/UIView+Ext.h \
+  /Users/seal/weightscale/WeighBean/General/Views/HTTextField.h \
+  /Users/seal/weightscale/WeighBean/General/Categories/UILabel+Ext.h \
+  /Users/seal/weightscale/WeighBean/Model/Models/Common/HTAppContext.h \
+  /Users/seal/weightscale/WeighBean/Model/Models/Common/HTUserData.h \
+  /Users/seal/weightscale/WeighBean/General/Notification/NotificationCenter.h \
+  /Users/seal/weightscale/WeighBean/General/Common/LoginControllerProtocol.h \
+  /Users/seal/weightscale/WeighBean/General/Views/HTNavigationBar.h \
+  /Users/seal/weightscale/WeighBean/Sections/CheckInImageAlbumViewController.h \
+  /Users/seal/weightscale/WeighBean/Sections/CheckInImagePickerCell.h \
+  /Users/seal/weightscale/WeighBean/Sections/CheckInPickResultViewController.h \
+  /Users/seal/weightscale/WeighBean/Model/ModelHandlers/UploadDakaModelHandler.h \
+  /Users/seal/weightscale/WeighBean/Model/ModelObjects/UploadDakaResponse.h \
+  /Users/seal/weightscale/WeighBean/Model/Models/UploadDakaModel.h \
+  /Users/seal/weightscale/WeighBean/Library/ImageCropView.h \
+  /Users/seal/weightscale/WeighBean/Library/FXBlurView.h
