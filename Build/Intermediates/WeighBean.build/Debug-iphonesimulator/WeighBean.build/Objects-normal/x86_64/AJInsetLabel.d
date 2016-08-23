17c49ddd9e5cc45ff297158bdbc3156a51d506df
seal <zwzzebra@gmail.com>
2016-08-24 00:35:43 +0800

    15

---
+++ Build/Intermediates/WeighBean.build/Debug-iphonesimulator/WeighBean.build/Objects-normal/x86_64/AJInsetLabel.d
@@ -0,0 +1,2 @@
+dependencies: /Users/seal/weightscale/WeighBean/Library/AJInsetLabel.m \
+  /Users/seal/weightscale/WeighBean/Library/AJInsetLabel.h
---
+++ Build/Intermediates/WeighBean.build/Debug-iphonesimulator/WeighBean.build/Objects-normal/x86_64/AJTagView.d
@@ -0,0 +1,3 @@
+dependencies: /Users/seal/weightscale/WeighBean/Library/AJTagView.m \
+  /Users/seal/weightscale/WeighBean/Library/AJTagView.h \
+  /Users/seal/weightscale/WeighBean/Library/AJInsetLabel.h
