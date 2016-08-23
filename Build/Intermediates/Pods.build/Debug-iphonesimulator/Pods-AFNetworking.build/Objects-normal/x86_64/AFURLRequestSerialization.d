1cd00ab8df062dd80437ec81da22da9122a780e7
seal <zwzzebra@gmail.com>
2016-08-24 00:42:32 +0800

    s

--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/x86_64/AFURLRequestSerialization.d
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/x86_64/AFURLRequestSerialization.d
@@ -1,5 +1,5 @@
 dependencies: \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.m \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.h
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.m \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.h
--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/x86_64/AFURLResponseSerialization.d
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/x86_64/AFURLResponseSerialization.d
@@ -1,5 +1,5 @@
 dependencies: \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.m \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.h
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.m \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.h
