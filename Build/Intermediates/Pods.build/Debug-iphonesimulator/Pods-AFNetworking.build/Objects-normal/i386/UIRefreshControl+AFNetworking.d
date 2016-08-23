6fd2c036895eb0dd72da955bf717bc83a77f9811
seal <zwzzebra@gmail.com>
2016-08-24 00:40:52 +0800

    save

--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/i386/UIRefreshControl+AFNetworking.d
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/i386/UIRefreshControl+AFNetworking.d
@@ -1,12 +1,12 @@
 dependencies: \
-  /Users/seal/WeighBean/Pods/AFNetworking/UIKit+AFNetworking/UIRefreshControl+AFNetworking.m \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/UIKit+AFNetworking/UIRefreshControl+AFNetworking.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperation.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLConnectionOperation.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFSecurityPolicy.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLSessionManager.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFNetworkReachabilityManager.h
+  /Users/seal/weightscale/Pods/AFNetworking/UIKit+AFNetworking/UIRefreshControl+AFNetworking.m \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
+  /Users/seal/weightscale/Pods/AFNetworking/UIKit+AFNetworking/UIRefreshControl+AFNetworking.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperation.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLConnectionOperation.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFSecurityPolicy.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLSessionManager.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFNetworkReachabilityManager.h
--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/x86_64/AFHTTPRequestOperationManager.d
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-AFNetworking.build/Objects-normal/x86_64/AFHTTPRequestOperationManager.d
@@ -1,11 +1,11 @@
 dependencies: \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperationManager.m \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
-  /Users/seal/WeighBean/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperationManager.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperation.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLConnectionOperation.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFSecurityPolicy.h \
-  /Users/seal/WeighBean/Pods/AFNetworking/AFNetworking/AFNetworkReachabilityManager.h
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperationManager.m \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods-AFNetworking/Pods-AFNetworking-prefix.pch \
+  /Users/seal/weightscale/Pods/Target\ Support\ Files/Pods/Pods-environment.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperationManager.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFHTTPRequestOperation.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLConnectionOperation.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLRequestSerialization.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFSecurityPolicy.h \
+  /Users/seal/weightscale/Pods/AFNetworking/AFNetworking/AFNetworkReachabilityManager.h
