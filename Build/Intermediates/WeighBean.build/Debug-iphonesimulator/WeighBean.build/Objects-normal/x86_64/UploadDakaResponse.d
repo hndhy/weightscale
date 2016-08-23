a8646b97d642ac59456317ff50b6f6d81c653601
seal <zwzzebra@gmail.com>
2016-08-24 00:22:58 +0800

    更新资源4

---
+++ Build/Intermediates/WeighBean.build/Debug-iphonesimulator/WeighBean.build/Objects-normal/x86_64/UploadDakaResponse.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/seal/weightscale/WeighBean/Model/ModelObjects/UploadDakaResponse.m \
+  /Users/seal/weightscale/WeighBean/Model/ModelObjects/UploadDakaResponse.h \
+  /Users/seal/weightscale/WeighBean/Model/ModelObjects/BaseResponse.h
---
+++ Build/Intermediates/WeighBean.build/Debug-iphonesimulator/WeighBean.build/Objects-normal/x86_64/UserDataModel.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/seal/weightscale/WeighBean/Model/ModelObjects/Login/UserDataModel.m \
+  /Users/seal/weightscale/WeighBean/Model/ModelObjects/Login/UserDataModel.h
